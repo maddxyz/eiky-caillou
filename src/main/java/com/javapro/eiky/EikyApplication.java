4fa55abc697038a25455910488603b136d1ba822
Imad E <enneiymy.i@gmail.com>
2019-03-02 14:23:03 +0100

    INIT Project

---
+++ src/main/java/com/javapro/eiky/EikyApplication.java
@@ -0,0 +1,13 @@
+package com.javapro.eiky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EikyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EikyApplication.class, args);
+    }
+
+}
